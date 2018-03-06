ae44cd5793d95a82b69ee5ca743003acfe6b2c97
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2018-03-07 01:00:20 +0200

    back to firefox :)

--- karma.conf.js
+++ karma.conf.js
@@ -67,8 +67,8 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: ['Firefox']
-        browsers: ['Chrome']
+        browsers: ['Firefox']
+        // browsers: ['Chrome']
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
